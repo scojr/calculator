94f70b00085df40f5fd75fda0344bdf8465de78d
scojr <180873402+scojr@users.noreply.github.com>
2024-10-12 17:46:52 -0500

    feat: add initial display functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // script.js
 
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
+
 const operators = {
   "+": add,
   "-": subtract,
@@ -9,24 +10,27 @@ const operators = {
   "/": divide,
 }
 
+const numButtons = buttons.querySelectorAll(".numButton");
+const operationButtons = buttons.querySelectorAll(".operationButton");
+
+
+numButtons.forEach(item => {
+  item.addEventListener("click", () => display.textContent = firstNumber += (item.textContent));
+});
+
 function operate(firstN, oper, secondN) {
   return operators[oper](firstN, secondN);
 }
-
 function add(numOne, numTwo) {
   return numOne + numTwo;
 }
-
 function subtract(numOne, numTwo) {
   return numOne - numTwo;
-
 }
-
 function multiply(numOne, numTwo) {
   return numOne * numTwo;
 
 }
-
 function divide(numOne, numTwo) {
   return numOne / numTwo;
 
