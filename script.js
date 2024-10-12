015f06fb4006ddf3649407b87f01e265cde3ff88
scojr <180873402+scojr@users.noreply.github.com>
2024-10-12 16:46:58 -0500

    feat: add basic math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+
+function add(numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+function substract(numOne, numTwo) {
+  return numOne - numTwo;
+
+}
+
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+
+}
+
+function divide(numOne, numTwo) {
+  return numOne / numTwo;
+
+}
