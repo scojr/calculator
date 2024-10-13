88fbda57593b912df8806c3f6fbcbd8d0140eafe
scojr <180873402+scojr@users.noreply.github.com>
2024-10-13 14:25:49 -0500

    feat: link up ui to calculator operation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // script.js
-
+let userInput = "";
 let firstNumber = "";
 let secondNumber = "";
+let userOperation = "";
 
 const operators = {
   "+": add,
@@ -12,10 +13,35 @@ const operators = {
 
 const numButtons = buttons.querySelectorAll(".numButton");
 const operationButtons = buttons.querySelectorAll(".operationButton");
-
+const equalsButton = buttons.querySelector("#equalsButton");
+const clearButton = buttons.querySelector("#clearButton");
 
 numButtons.forEach(item => {
-  item.addEventListener("click", () => display.textContent = firstNumber += (item.textContent));
+  item.addEventListener("click", () => display.textContent = userInput += (item.textContent));
+});
+
+operationButtons.forEach(item => {
+  item.addEventListener("click", () => {
+    firstNumber = userInput;
+    userInput = "";
+    display.textContent = "00000"
+    userOperation = (item.textContent);
+  })
+});
+
+equalsButton.addEventListener("click", () => {
+  if (firstNumber) {
+    secondNumber = userInput;
+    display.textContent = (operate(parseInt(firstNumber), userOperation, parseInt(secondNumber)))
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  userInput = "";
+  firstNumber = "";
+  secondNumber = "";
+  userOperation = "";
+  display.textContent = "00000"
 });
 
 function operate(firstN, oper, secondN) {
@@ -29,9 +55,7 @@ function subtract(numOne, numTwo) {
 }
 function multiply(numOne, numTwo) {
   return numOne * numTwo;
-
 }
 function divide(numOne, numTwo) {
   return numOne / numTwo;
-
 }
